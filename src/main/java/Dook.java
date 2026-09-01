3a6a8f9b932de9192efe6830e43670afb5c232dc
lololol8484 <227026282+lololol8484@users.noreply.github.com>
2026-09-01 17:05:08 +0800

    Rename chatbot and implement greeting and exit

---
+++ src/main/java/Dook.java
@@ -0,0 +1,16 @@
+public class Dook {
+    public static void main(String[] args) {
+        String banner = " ____   ___   ___  _  __\n"
+                + "|  _ \\ / _ \\ / _ \\| |/ /\n"
+                + "| | | | | | | | | | ' / \n"
+                + "| |_| | |_| | |_| | . \\ \n"
+                + "|____/ \\___/ \\___/|_|\\_\\\n";
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm Dook.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
