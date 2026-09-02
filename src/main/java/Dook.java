daa1b7d9bd2382e13d7367616bb7d603203933b5
lololol8484 <227026282+lololol8484@users.noreply.github.com>
2026-09-02 18:01:46 +0800

    Implement mark as done feature

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -6,7 +6,7 @@ public class Dook {
                                 + "\t| | | | | | | | | | ' / \n"
                                 + "\t| |_| | |_| | |_| | . \\ \n"
                                 + "\t|____/ \\___/ \\___/|_|\\_\\\n";
-    private static String[] tasks = new String[100];
+    private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
@@ -25,14 +25,35 @@ public class Dook {
 
             if(input.equals("list")) {
                 System.out.println("\t____________________________________________________________");
+                System.out.println("\tHere are the tasks in your list:");
                 for(int i = 0; i < taskCount; i++) {
-                    System.out.println("\t" + (i + 1) + ". " + tasks[i]);
+                    System.out.println("\t" + (i + 1) + ". [" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
                 System.out.println("\t____________________________________________________________\n");
                 continue;
             }
 
-            tasks[taskCount] = input;
+            if(input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber].markAsDone();
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tNice! I've marked this task as done:");
+                System.out.println("\t   [" + tasks[taskNumber].getStatusIcon() + "] " + tasks[taskNumber].getDescription());
+                System.out.println("\t____________________________________________________________\n");
+                continue;
+            }
+
+            if(input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber].markAsNotDone();
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tOK, I've marked this task as not done yet:");
+                System.out.println("\t   [" + tasks[taskNumber].getStatusIcon() + "] " + tasks[taskNumber].getDescription());
+                System.out.println("\t____________________________________________________________\n");
+                continue;
+            }
+
+            tasks[taskCount] = new Task(input);
             taskCount++;
             System.out.println("\t____________________________________________________________");
             System.out.println("\tAdded: " + input);
