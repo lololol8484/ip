ebc93ca5948b4fbb8d334e5676d4a5cad40edf3f
lololol8484 <227026282+lololol8484@users.noreply.github.com>
2026-09-02 15:37:27 +0800

    Implement echo feature

--- src/main/java/Dook.java
+++ src/main/java/Dook.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
 public class Dook {
+    static final String banner = "\t ____   ___   ___  _  __\n"
+                                + "\t|  _ \\ / _ \\ / _ \\| |/ /\n"
+                                + "\t| | | | | | | | | | ' / \n"
+                                + "\t| |_| | |_| | |_| | . \\ \n"
+                                + "\t|____/ \\___/ \\___/|_|\\_\\\n";
+
     public static void main(String[] args) {
-        String banner = " ____   ___   ___  _  __\n"
-                + "|  _ \\ / _ \\ / _ \\| |/ /\n"
-                + "| | | | | | | | | | ' / \n"
-                + "| |_| | |_| | |_| | . \\ \n"
-                + "|____/ \\___/ \\___/|_|\\_\\\n";
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
         System.out.println(banner);
-        System.out.println("Hello! I'm Dook.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\tHello! I'm Dook.");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________\n");
+
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            String input = in.nextLine();
+            if(input.equals("bye")) {
+                break;
+            }
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t" + input);
+            System.out.println("\t____________________________________________________________\n");
+        }
+
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________\n");
     }
 }
